3b0518bca41e5b802a71af6895521dab880e30b8
Krishna Patel <softeng.krishna@gmail.com>
2015-05-12 18:48:26 +0530

    efgbhjfg

--- app/src/main/java/com/app/invisivault/MainActivity.java
+++ app/src/main/java/com/app/invisivault/MainActivity.java
@@ -1,38 +1,119 @@
+/*
+ * Copyright 2013 - learnNcode (learnncode@gmail.com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package com.app.invisivault;
 
-import android.support.v7.app.ActionBarActivity;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import com.learnncode.mediachooser.MediaChooser;
+import com.learnncode.mediachooser.activity.BucketHomeFragmentActivity;
+import com.learnncode.mediachooser.activity.HomeFragmentActivity;
+
+public class MainActivity extends Activity {
+
+	Button folderViewButton;
+	Button fileViewButton;
+	GridView gridView;
+	MediaGridViewAdapter adapter;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE); 
+		setContentView(R.layout.activity_main);
+		folderViewButton = (Button)findViewById(R.id.folderButton);
+		fileViewButton = (Button)findViewById(R.id.fileButton);
+		gridView = (GridView)findViewById(R.id.gridView);
+		folderViewButton.setOnClickListener(clickListener);
+		fileViewButton.setOnClickListener(clickListener);
+
+		IntentFilter videoIntentFilter = new IntentFilter(MediaChooser.VIDEO_SELECTED_ACTION_FROM_MEDIA_CHOOSER);
+		registerReceiver(videoBroadcastReceiver, videoIntentFilter);
+
+		IntentFilter imageIntentFilter = new IntentFilter(MediaChooser.IMAGE_SELECTED_ACTION_FROM_MEDIA_CHOOSER);
+		registerReceiver(imageBroadcastReceiver, imageIntentFilter);
+
+	}
 
 
-public class MainActivity extends ActionBarActivity {
+	OnClickListener clickListener = new OnClickListener() {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+		@Override
+		public void onClick(View view) {
+			if(view == folderViewButton){
+				MediaChooser.setSelectionLimit(20);
+				Intent intent = new Intent(MainActivity.this, BucketHomeFragmentActivity.class);
+				startActivity(intent);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+			}else {
+				Intent intent = new Intent(MainActivity.this, HomeFragmentActivity.class);
+				startActivity(intent);
+			}
+		}
+	};
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+	BroadcastReceiver videoBroadcastReceiver = new BroadcastReceiver() {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+		@Override
+		public void onReceive(Context context, Intent intent) {
 
-        return super.onOptionsItemSelected(item);
-    }
+			Toast.makeText(MainActivity.this, "yippiee Video ", Toast.LENGTH_SHORT).show();
+			Toast.makeText(MainActivity.this, "Video SIZE :" + intent.getStringArrayListExtra("list").size(), Toast.LENGTH_SHORT).show();
+			setAdapter(intent.getStringArrayListExtra("list"));
+		}
+	};
+
+
+	BroadcastReceiver imageBroadcastReceiver = new BroadcastReceiver() {
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			Toast.makeText(MainActivity.this, "yippiee Image ", Toast.LENGTH_SHORT).show();
+			Toast.makeText(MainActivity.this, "Image SIZE :" + intent.getStringArrayListExtra("list").size(), Toast.LENGTH_SHORT).show();
+			setAdapter(intent.getStringArrayListExtra("list"));
+		}
+	};
+
+	@Override
+	protected void onDestroy() {
+		unregisterReceiver(imageBroadcastReceiver);
+		unregisterReceiver(videoBroadcastReceiver);
+		super.onDestroy();
+	}
+
+	private void setAdapter( List<String> filePathList) {
+		if(adapter == null){
+			adapter = new MediaGridViewAdapter(MainActivity.this, 0, filePathList);
+			gridView.setAdapter(adapter);
+		}else{
+			adapter.addAll(filePathList);
+			adapter.notifyDataSetChanged();
+		}
+	}
 }
